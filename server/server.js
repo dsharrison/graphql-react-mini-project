9b4c7dd9aca57de38712873b25e88191508bdc92
Derrek Harrison <derrek.s.harrison@gmail.com>
2018-03-12 13:51:39 -0700

    Switch to custom sandbox db

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://lyrical2:lyrical2@ds261838.mlab.com:61838/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
